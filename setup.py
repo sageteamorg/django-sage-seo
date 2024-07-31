d607d9d75448a7d8ea990a2fab103a765cc6240d
Sepehr Akbarzadeh <sa.goldeneagle@gmail.com>
2024-08-01 01:11:58 +0330

    fix(version): update version and add readthedocs configuration

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="django_sage_seo",
-    version="0.2.3",
+    version="0.2.4",
     author="Sepehr Akbarzadeh",
     author_email="sepehr@sageteam.org",
     description="SEO tools for Django",
